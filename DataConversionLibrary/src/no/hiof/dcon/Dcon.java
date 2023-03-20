3d763ca5bc7d974556fcf2c815c8da06abe51570
Marius Christian Aastebøl <mariusca@hiof.no>
2023-03-20 20:19:34 +0100

    Added more Javadoc descriptions for the Dcon class.

--- DataConversionLibrary/src/no/hiof/dcon/Dcon.java
+++ DataConversionLibrary/src/no/hiof/dcon/Dcon.java
@@ -1,35 +1,78 @@
 package no.hiof.dcon;
 
+/**
+ * A general class which has many methods for dataconversion with both Json And Xml.
+ */
 public class Dcon {
     protected Dcon() {
     }
 
+    /**
+     * Reads a provided file and turns the result into a string.
+     * @param filename  A string with the full name of a file. For example "file.txt"
+     * @return A string with the contents of the file.
+     */
     public String readFileAsString(String filename) {
         return null;
     }
 
-    public String writeToFile(String fileName) {
-        return null;
-    }
-
+    /**
+     * Checks if two values can be found in the same object in a Dcon list.
+     * @param list      A DconList object which contains Dcon Objects with values.
+     * @param value1    The first value to look for.
+     * @param value2    The Second value to look for, if the first one exists.
+     * @return          True or false, depending on whether both the values were found.
+     */
     public static boolean isBothInSameObject(DconList list, String value1, String value2) {
         return false;
     }
 
+    /**
+     * Appends a file with a given string value.
+     * @param filename  A string with the full name of a file. For example "file.txt"
+     * @param value     The string value that shall be appended on a new line.
+     */
     public void appendFileNewLine(String filename, String value) {
     }
 
+    /**
+     * Looks through a list and searches for a specific key. Takes the value for every key and finds the average.
+     * @param list      A DconList object which contains Dcon Objects with keys and values.
+     * @param keyName
+     * @return
+     */
     public double calculateAverageProperty(DconList list, String keyName) {
         return 0;
     }
 
     // Json
-    public String deserializeJson(String fileName, Class className) {
+
+    /**
+     * Deserializes (also called decode) a file into a given class.
+     * @param fileName      A string with the full name of a file. For example "file.txt"
+     * @param <className>   The name of a class that you want to convert the file into.
+     *                      Needs to have corresponding property values to the keys in the file.
+     * @return              An object of the provided class.
+     */
+    public <className> Object deserializeJson(String fileName) {
         return null;
     }
 
+    /**
+     * Creates a general DconObject based on a Json file.
+     * @param fileName  A string with the full name of a file. For example "file.txt"
+     * @return  A DconObject instance with corresponding keys and values to the contents of the file.
+     */
     public DconObject createObjectFromJson(String fileName) {
         return null;
+    }
+
+    /**
+     * Writes a dcon object to a provided file in Json format.
+     * @param fileName      The desired name of a file.
+     * @param dconObject    The object you want to write to the file.
+     */
+    public void writeObjectToJsonFile(String fileName, DconObject dconObject) {
     }
 
     public static boolean validateJson(String fileName){
@@ -51,6 +94,14 @@ public class Dcon {
 
     public DconObject createObjectFromXml(String fileName) {
         return null;
+    }
+
+    /**
+     * Writes a dcon object to a provided file in Xml format.
+     * @param fileName      The desired name of a file.
+     * @param dconObject    The object you want to write to the file.
+     */
+    public void writeObjectToXmlFile(String fileName, DconObject dconObject) {
     }
 
     public boolean validateXml(String fileName) {
