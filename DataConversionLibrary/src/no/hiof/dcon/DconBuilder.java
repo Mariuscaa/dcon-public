004e24f9568d671961a950e8ffc325061196413f
Bharati Mainali <bharatimainali@gmail.com>
2023-03-21 10:43:36 +0100

    new committed for DconBuilder.

---
+++ DataConversionLibrary/src/no/hiof/dcon/DconBuilder.java
@@ -0,0 +1,35 @@
+package no.hiof.dcon;
+
+public class DconBuilder {
+
+    public Dcon createDconBuilder(){
+        /**
+         * createDconBuilder() is the method that will create an instance of the DconBuilder class.
+         */
+        return null;
+    }
+
+    public static String serializeDconBuilder(DconBuilder dconbuilder){
+        /**
+         * "serializeDconBuilder() is the static method that will serialize a DconBuilder object using the dconbuilder.Serialize() method.
+         */
+        return serializedDconBuilder;
+    }
+
+    public static DataConverterDCON deserializeJsonToDataConverterDCON(String json){
+        /**
+         * "deserializeJsonToDataConverterDCON() is the static method that will deserialize a JSON string to a DataConverterDCON object using the DataConverterDCON.deserialize() method.
+         */
+        return dataConverterDCON;
+    }
+
+    public static DconBuilder setDefaultDataFormat(String dataFormat){
+        /**
+         * The method should take a single parameter of type String, which will represent the default data format to be set.
+         */
+        return builder;
+    }
+
+}
+
+
